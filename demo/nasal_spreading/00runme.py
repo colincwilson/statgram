8518cc47a009cd74e921e505a73a6f37cf26fcc5
Colin Wilson <colin.chris.wilson@gmail.com>
2020-07-10 08:10:28 -0400

    update nasal spreading demo with anticipatory option

--- demo/nasal_spreading/00runme.py
+++ demo/nasal_spreading/00runme.py
@@ -1,12 +1,15 @@
 # -*- coding: utf-8 -*-
 
-import argparse, re, sys
+import re, sys
 import itertools
 sys.path.append('../../../fst_util')
 sys.path.append('../..')
 from statgram.harmony import Node, HGStat, OTStat
 from fst_util import fst_config, fst_util
 FST, Transition = fst_util.FST, fst_util.Transition
+
+Spread_direction = {0:'LR->', 1:'<-RL'}[0]
+Stat = {0:HGStat, 1:OTStat}[0]
 
 
 # # # # # # # # # #
@@ -20,7 +23,7 @@ fst_config.Sigma = {''.join(x) for x in Sigma}
 print(f'|Sigma| = {len(fst_config.Sigma)}')
 #print(fst_config.Sigma)
 
-def pretty_print(form):
+def pretty_print_spans(form):
     # x( -> (x+
     form = re.sub('(\S+)[(]', '(\\1+', form)
     # x) -> x+)
@@ -84,12 +87,22 @@ fst_util.draw(M_tier, 'Tier_nasal.dot')
 
 # Left-context machine with one-segment history
 M_left = fst_util.left_context_acceptor(fst_config.Sigma, 1)
-print(f'M_left: {len(M_left.Q)} states, {len(M_tier.T)} transitions')
+print(f'M_left: {len(M_left.Q)} states, {len(M_left.T)} transitions')
 fst_util.draw(M_left, 'Left_context.dot')
 # dot -Tpdf Left_context.dot > Left_context.pdf
 
-# Intersection of M_tier and M_left
-Gen = fst_util.intersect(M_tier, M_left)
+# Right-context machine with one-segment lookahead
+M_right = fst_util.right_context_acceptor(fst_config.Sigma, 1)
+print(f'M_right: {len(M_right.Q)} states, {len(M_right.T)} transitions')
+fst_util.draw(M_right, 'Right_context.dot')
+# dot -Tpdf Right_context.dot > Right_context.pdf
+
+
+# Intersection of M_tier and M_left or M_right
+if Spread_direction == 'LR->':
+    Gen = fst_util.intersect(M_tier, M_left)
+else:
+    Gen = fst_util.intersect(M_tier, M_right)
 Gen = fst_util.map_states(Gen, lambda q: (q[0], q[1][0]))
 print(f'Gen: {len(Gen.Q)} states, {len(Gen.T)} transitions')
 fst_util.draw(Gen, 'Gen_nasal.dot')
@@ -102,10 +115,10 @@ def NasN(t):
     label = t.olabel
     if re.search('[N]', label):
         #if re.search('[-]', label): # N must be [+nasal]
-        if not re.search('1', label): # N must be head of +nasal span
-            return ('NasN', -1)
-        else:
+        if re.search('1', label): # N must be head of +nasal span
             return ('NasN', +1)
+        else:
+            return ('NasN', -1)
     return ('NasN', 0)
 
 def NoNasObs(t):
@@ -126,39 +139,68 @@ def NoNasVoc(t):
             return ('NoNasVoc', -1)
     return ('NoNasVoc', 0)
 
-def SpreadR(t):
+def SpreadNasR(t):
     left_context, label = \
         t.src[1], t.olabel
     if re.search('[)|]', left_context): # I shoulda been a dependent
-        return ('SpreadR', -1)
+        return ('SpreadNasR', -1)
     if re.search('[(+]', left_context) \
         and re.search('0[+)]', label):
-            return ('SpreadR', +1)
-    return ('SpreadR', 0)
+            return ('SpreadNasR', +1)
+    return ('SpreadNasR', 0)
 
-def SyllStruc(t):
+def SpreadNasL(t):
+    right_context, label = \
+        t.dest[1], t.olabel
+    if re.search('[(|]', right_context): # I shoulda been a dependent
+        return ('SpreadNasL', -1)
+    if re.search('[)+]', right_context) \
+        and re.search('0[+(]', label):
+            return ('SpreadNasL', +1)
+    return ('SpreadNasL', 0)
+
+def SyllStrucR(t):
     left_context, label = \
         t.src[1], t.olabel
     if re.search('[TSNG]', left_context):
         if re.search('[TSNG]', label): # no clusters
-            return ('SyllStruc', -1)
+            return ('SyllStrucR', -1)
     if re.search('[V]', left_context):
         if re.search('[V]', label): # no hiatus
-            return ('SyllStruc', -1)
-    return ('SyllStruc', 0)
+            return ('SyllStrucR', -1)
+    return ('SyllStrucR', 0)
 
+def SyllStrucL(t):
+    right_context, label = \
+        t.dest[1], t.olabel
+    if re.search('[TSNG]', right_context):
+        if re.search('[TSNG]', label): # no clusters
+            return ('SyllStrucL', -1)
+    if re.search('[V]', right_context):
+        if re.search('[V]', label): # no hiatus
+            return ('SyllStrucL', -1)
+    return ('SyllStrucL', 0)
 
 # # # # # # # # # #
 # Eval
-Con = [NasN, NoNasObs, NoNasVoc, SpreadR, SyllStruc]
+Con = [NasN, NoNasObs, NoNasVoc]
 weights = { 'NasN': 3.0,
             'NoNasObs': 3.0,
-            'NoNasVoc': 1.0,
-            'SpreadR': 2.0,
-            'SyllStruc': 10.0 }
-markup = {} # map transitions to mark sets
+            'NoNasVoc': 1.0 }
+if Spread_direction == 'LR->':
+    Con += [SpreadNasR, SyllStrucR]
+    weights['SpreadNasR'] = 2.0
+    weights['SyllStrucR'] = 10.0
+else:
+    Con += [SpreadNasL, SyllStrucL]
+    weights['SpreadNasL'] = 2.0
+    weights['SyllStrucL'] = 10.0
+
+# Assign marks to transitions
+markup = {}
 for t in Gen.T:
-    if t.olabel == fst_config.end_delim:
+    if t.olabel == fst_config.begin_delim \
+        or t.olabel == fst_config.end_delim:
         continue
     marks = set()
     for constraint in Con:
@@ -167,9 +209,12 @@ for t in Gen.T:
             marks.add(mark)
     markup[t] = Node(t, marks)
 
-_, nodes_ill = HGStat(markup.values(), weights)
-#_, nodes_ill = OTStat(markup.values(), weights)
+_, nodes_ill = Stat(markup.values(), weights)
 for node in nodes_ill: # xxx copy Gen first
+    if ('NoNasVoc', -1) in node.marks \
+        and ('SpreadNasL', +1) in node.marks \
+        and ('SyllStrucL', -1) not in node.marks:
+        print(node.t, node.marks)
     Gen.T.remove(node.t)
 Lang = fst_util.trim(Gen)
 print(f'Lang: {len(Lang.Q)} states, {len(Lang.T)} transitions')
@@ -177,10 +222,10 @@ fst_util.draw(Lang, 'Lang_nasal.dot')
 # dot -Tpdf Lang_nasal.dot > Lang_nasal.pdf
 
 # # # # # # # # # #
-# Words
+# Outputs
 Output = fst_util.intersect(Lang, fst_util.trellis(4))
 fst_util.draw(Output, 'Output_nasal.dot')
 outputs = fst_util.accepted_strings(Lang, 4)
-outputs = { pretty_print(x) for x in outputs }
+outputs = { pretty_print_spans(x) for x in outputs }
 print('All legal words with <= 4 segments:')
 print(outputs)
